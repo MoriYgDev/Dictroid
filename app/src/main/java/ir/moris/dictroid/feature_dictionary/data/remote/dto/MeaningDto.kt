64eb46ad1998933eb377a4ec73951b2f7affd84e
morteza yaghoobi <morteza.yaghoobi1375@gmail.com>
2022-04-11 10:41:06 +0430

    add MeaningDto.kt.kt mapper to Meaning object

--- app/src/main/java/ir/moris/dictroid/feature_dictionary/data/remote/dto/MeaningDto.kt
+++ app/src/main/java/ir/moris/dictroid/feature_dictionary/data/remote/dto/MeaningDto.kt
@@ -1,6 +1,12 @@
 package ir.moris.dictroid.feature_dictionary.data.remote.dto
 
+import ir.moris.dictroid.feature_dictionary.domain.model.Meaning
+
 data class MeaningDto(
     val definitions: List<DefinitionDto>,
     val partOfSpeech: String
-)
+) {
+    fun toMeaning(): Meaning {
+        return Meaning(definitions.map { it.toDefinition() }, partOfSpeech)
+    }
+}
---
+++ app/src/main/java/ir/moris/dictroid/feature_dictionary/domain/model/Meaning.kt
@@ -0,0 +1,3 @@
+package ir.moris.dictroid.feature_dictionary.domain.model
+
+data class Meaning(val definitions : List<Definition> , val partOfSpeech : String)
