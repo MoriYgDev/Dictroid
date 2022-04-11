44ade914d40717bdba23ec0083bd48d232f12946
morteza yaghoobi <morteza.yaghoobi1375@gmail.com>
2022-04-11 11:40:41 +0430

    add type converters to database

--- app/src/main/java/ir/moris/dictroid/feature_dictionary/data/local/WordInfoDatabase.kt
+++ app/src/main/java/ir/moris/dictroid/feature_dictionary/data/local/WordInfoDatabase.kt
@@ -2,11 +2,13 @@ package ir.moris.dictroid.feature_dictionary.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import ir.moris.dictroid.feature_dictionary.data.local.entity.WordInfoEntity
 
 @Database(
     entities = [WordInfoEntity::class] , version = 1
 )
+@TypeConverters(Converters::class)
 abstract class WordInfoDatabase : RoomDatabase(){
 
     abstract val dao : WordInfoDao
