e5f1f78b23622d2f5c75a05343021f1da6f540e4
morteza yaghoobi <morteza.yaghoobi1375@gmail.com>
2022-04-11 10:46:32 +0430

    add WordInfoDto.kt mapper to WordInfo object

--- app/src/main/java/ir/moris/dictroid/feature_dictionary/data/remote/dto/WordInfoDto.kt
+++ app/src/main/java/ir/moris/dictroid/feature_dictionary/data/remote/dto/WordInfoDto.kt
@@ -1,9 +1,15 @@
 package ir.moris.dictroid.feature_dictionary.data.remote.dto
 
+import ir.moris.dictroid.feature_dictionary.domain.model.WordInfo
+
 data class WordInfoDto(
     val meanings: List<MeaningDto>,
     val origin: String,
     val phonetic: String,
     val phonetics: List<PhoneticDto>,
     val word: String
-)
+){
+    fun toWordInfo():WordInfo{
+        return WordInfo(meanings.map { it.toMeaning() }, origin, phonetic, word)
+    }
+}
---
+++ app/src/main/java/ir/moris/dictroid/feature_dictionary/domain/model/WordInfo.kt
@@ -0,0 +1,9 @@
+package ir.moris.dictroid.feature_dictionary.domain.model
+
+
+data class WordInfo(
+    val meanings: List<Meaning>,
+    val origin: String,
+    val phonetic: String,
+    val word: String
+)
